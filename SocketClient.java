7564db85deee1814cc57e0d53412cf34893c976c
Jarl André Hübenthal <jarl.andre@gmail.com>
2012-05-24 22:41:41 +0200

    changing SocketClient

--- SocketClient.java
+++ SocketClient.java
@@ -38,7 +38,7 @@ public class SocketClient {
 						out = new PrintWriter(echoSocket.getOutputStream(), true);
 						in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
 					} catch (UnknownHostException e) {
-						System.err.println("Don't know about host: taranis.");
+						System.err.println("Don't know about host: localhost.");
 						System.exit(1);
 					} catch (IOException e) {
 						System.err.println("Couldn't get I/O for " + "the connection to: localhost.");
